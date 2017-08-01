d86156dec9ebcf9ee693f2925c8f24beb0aa3f90
Colin <silvercpb@gmail.com>
2017-08-01 14:28:44 -0400

    Placeholder markers are in

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ##Sources for code:
 CSS-Only Nav Menu: https://www.sitepoint.com/pure-css-off-screen-navigation-menu/
+A lot of the map code: Udacity API Coursework (Google Maps API)
--- js/map.js
+++ js/map.js
@@ -1,8 +1,69 @@
+var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
 function initMap() {
 	// Constructor creates a new map - only center and zoom are required.
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 35.658424, lng: -81.235107},
-		zoom: 16,
-		mapTypeControl: false
+		zoom: 14,
+		streetViewControl: false,
+		mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.TOP_CENTER
+    }
 	});
+
+	// These are the locations of the stuff in my hometown.
+	var locations = [
+		{title: 'Little Caesers', location: {lat: 35.6593, lng: -81.2289}},
+		{title: 'Western Steer', location: {lat: 35.6589, lng: -81.2306}},
+		{title: 'Blue Moon', location: {lat: 35.6637, lng: -81.2224}},
+		{title: 'Pin Station', location: {lat: 35.6629, lng: -81.2297}},
+		{title: 'South Newton Elementary', location: {lat: 35.6543, lng: -81.2288}}
+	];
+
+	var largeInfowindow = new google.maps.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+
+	// The following group uses the location array to create an array of markers on initialize.
+	for (var i = 0; i < locations.length; i++) {
+		// Get the position from the location array.
+		var position = locations[i].location;
+		var title = locations[i].title;
+		// Create a marker per location, and put into markers array.
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+		// Push the marker to our array of markers.
+		markers.push(marker);
+		// Create an onclick event to open an infowindow at each marker.
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfowindow);
+		});
+		bounds.extend(markers[i].position);
+	}
+	// Extend the boundaries of the map for each marker
+	map.fitBounds(bounds);
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick',function(){
+			infowindow.setMarker = null;
+		});
+	}
 }
